57153cf46d008f03d4622b1a8215f69528d528ff
Jake L <jakelovitz@gmail.com>
2019-04-03 17:34:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,7 @@
 def using_push (countries_in_western_africa, next_country)
   countries_in_western_africa.push (next_country)
 end
+
+def using_unshift (neighborhoods_in_northwest_brooklyn, new_neigbhorhood)
+  neighborhoods_in_northwest_brooklyn.unshift (new_neigbhorhood)
+end
