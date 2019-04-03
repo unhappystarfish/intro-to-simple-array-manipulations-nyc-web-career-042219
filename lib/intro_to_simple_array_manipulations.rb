2879684f744bbde98f706f12ad6df5b8d0c1353a
Jake L <jakelovitz@gmail.com>
2019-04-03 17:59:36 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push (countries_in_western_africa, next_country)
+  countries_in_western_africa.push (next_country)
+end
+
+def using_unshift (neighborhoods_in_northwest_brooklyn, new_neigbhorhood)
+  neighborhoods_in_northwest_brooklyn.unshift (new_neigbhorhood)
+end
+
+def using_pop (great_hits_of_the_nineties)
+  great_hits_of_the_nineties.pop
+end
+
+def pop_with_args (chars_in_game_of_thrones)
+  chars_in_game_of_thrones.pop(2)
+end
+
+def using_shift (my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def shift_with_args (ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+
+def using_concat (my_favorite_things, more_favs)
+  my_favorite_things.concat (more_favs)
+end
+
+def using_insert (list_of_esoteric_programming_languages, another_esoteric_language)
+  list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
+end
+
+def using_uniq (captain_planet_and_the_planeteers)
+  captain_planet_and_the_planeteers.uniq
+end
+
+def using_flatten (private_colleges_in_newyork)
+  private_colleges_in_newyork.flatten
+end
+
+def using_delete (instructors, string)
+  instructors.delete(string)
+end
+
+def using_delete_at (famous_robots, i)
+  famous_robots.delete_at (i)
+end
